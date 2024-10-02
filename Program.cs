7d5c8f98590d55b069b45746e04d883dc6b8ffeb
Alice <156319702+AliceHWD@users.noreply.github.com>
2024-10-02 10:16:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_exceções
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try //Bloco onde faz a tentativa
+            {
+                string[] linhas = File.ReadAllLines("Yo.txt");
+
+                foreach (string linha in linhas)
+                {
+                    Console.WriteLine(linha);
+                }
+            }
+            catch(FileNotFoundException especifico) //Se der um erro epecífico vai tratar ele aqui
+            {
+                Console.WriteLine("Erro específico | " + especifico.Message);
+            }
+            catch(Exception ex) //Bloco onde passa se der erro É GENÉRICO
+            {
+                Console.WriteLine(ex.Source);
+                Console.WriteLine(ex.Message);
+            }
+            finally //Aqui vai passar independente se der erro ou não
+            {
+                Console.WriteLine("Bloco finally");
+            }
+
+            Console.WriteLine("Chega até aqui");
+
+            Console.ReadKey();
+            
+
+
+
+        }
+    }
+}
